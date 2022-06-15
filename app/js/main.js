7c799246a9c61e89ec5a79c5a2cb46fd64424932
Atabegovv <atabegov98@mail.ru>
2022-06-15 13:57:09 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,17 @@ $(function () {
 
     var mixer = mixitup('.products__inner-box'); 
 
+    $(".rating__star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+    });
+
 });
